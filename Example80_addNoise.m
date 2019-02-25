51898dccff98de965656f4fb1d0ef3b4f52d9428
Kiko Rul·lan <frullan@cs.ucl.ac.uk>
2019-02-25 14:22:46 +0000

    Example 80 & 81 stochastic

---
+++ Example80_addNoise.m
@@ -0,0 +1,41 @@
+% Read data from files
+cd /cs/research/medim/projects2/projects/frullan/Documents/HighFreqCode/Examples/Ex80_3D_veins_subsampled;
+
+clear all;
+close all;
+
+
+% Import data
+filenameData = './input_data/forwardSignal_reference_3600sensors.dat';
+timeSignal = importdata(filenameData, ' ', 0);
+timeRT = timeSignal(1, :);
+inputRT = timeSignal(2:end, :);
+% Plot
+figure;
+imagesc(inputRT);
+box on;
+colorbar();
+
+%==================================================
+% ADD RANDOM NOISE
+%==================================================
+[nSensors, signalLength] = size(inputRT);
+% Random number generator
+rng(1);
+noise = normrnd(0,0.05,[nSensors,signalLength]);
+
+
+% Add noise
+inputRT_noisy = inputRT + inputRT.*noise;
+
+% Plot noise
+figure;
+plot(noise(1, :));
+% Plot input
+figure;
+plot(inputRT(1, :), 'LineWidth', 1.5);
+hold on;
+plot(inputRT_noisy(1, :), 'Color', 'r');
+% Write output
+timeSignal_noisy = [timeRT; inputRT_noisy];
+dlmwrite('./input_data/forwardSignal_reference_noisy5_3600sensors.dat', timeSignal_noisy, 'delimiter', ' ');
